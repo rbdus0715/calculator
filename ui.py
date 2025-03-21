b06a730edfc025ba66dc8b944012c67545c76aae
rbdus0715 <rbdus0715@gmail.com>
2025-03-22 01:34:44 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,7 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
+                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel,
+                             QLineEdit, QComboBox)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QDate, Qt
+from PyQt5 import QtCore
 
 class View(QWidget):
 
@@ -18,8 +21,21 @@ class View(QWidget):
         self.btn1 = QPushButton('Message', self)
         self.btn2 = QPushButton('Clear', self)
 
+        self.le1 = QLineEdit('0', self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le2 = QLineEdit('0', self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+        self.cb = QComboBox(self)
+        self.cb.addItems(['+', '-', '*', '/'])
+
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox = QHBoxLayout()
         hbox.addStretch(1)
+        vbox.addLayout(hbox_formular)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
 
