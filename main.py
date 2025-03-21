cce7a2ea875f9c02158ca54470e0ff00ac4ac181
rbdus0715 <rbdus0715@gmail.com>
2025-03-21 22:20:35 +0900

    Add icon.webp file and push button with handler function in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
 
@@ -8,9 +9,23 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1) # 버튼 위치
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
         self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.webp'))
         self.resize(256, 256)
         self.show()
+
+    def activateMessage(self):
+        QMessageBox.information(self, "information", "Button clicked!")
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
